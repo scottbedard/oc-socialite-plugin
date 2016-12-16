3d8d1b051080a47e05f0426108a691593c9f07bb
Scott Bedard <scottbedard90@gmail.com>
2016-12-16 09:01:44 -0700

    Setting up the basic form fields

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -20,9 +20,16 @@ return[
     'settings' => [
         'label' => 'Socialite settings',
         'description' => 'Manage authentication settings.',
+        'facebook' => [
+            'is_enabled_comment' => 'Turning this off will disable authentication with Facebook.',
+            'is_enabled' => 'Facebook authentication',
+            'id' => 'App ID',
+            'secret' => 'App Secret',
+            'tab' => 'Facebook',
+        ],
         'fields' => [
-            'is_enabled' => 'Socialite API',
             'is_enabled_comment' => 'Turning this off will disable all third-party authentication.',
+            'is_enabled' => 'Socialite authentication',
         ],
     ],
 ];
